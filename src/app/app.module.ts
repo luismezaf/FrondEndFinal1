1dca923b865c7c2d3fc047f3759e3aac545ba06e
David Nuñez <nundavid99@gmail.com>
2021-11-20 07:32:25 -0300

    se renombra componente de productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,16 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
-import { ProductosComponent } from "./components/productos/productos.component";
 import { ReportsComponent } from "./components/reports/reports.component";
 
 import { MatTableModule } from "@angular/material/table";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatInputModule } from "@angular/material/input";
+import { ProductsComponent } from "./components/products/products.component";
 
 // import { ProductosService } from "./services/productos.service";
 @NgModule({
-  declarations: [AppComponent, ReportsComponent, ProductosComponent],
+  declarations: [AppComponent, ReportsComponent, ProductsComponent],
   imports: [
     BrowserModule,
     MatInputModule,
---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "products-root",
+  templateUrl: "./products.component.html",
+  styleUrls: ["./products.component.css"]
+})
+export class ProductsComponent {
+  ngOnInit() {}
+}
