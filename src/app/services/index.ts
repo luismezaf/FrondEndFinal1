a58560180af444ae9026b60afbb277df1d3e74eb
luismezaf <44320290+luismezaf@users.noreply.github.com>
2021-11-20 23:45:17 -0300

    Corrección de endpoint

--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -1,2 +1,2 @@
-export const mainEndpoint = "https://7174-181-120-136-95.ngrok.io";
-//'https://my-json-server.typicode.com/davinun99/dummy-json-server/';
+export const mainEndpoint =
+  "https://my-json-server.typicode.com/davinun99/dummy-json-server";
