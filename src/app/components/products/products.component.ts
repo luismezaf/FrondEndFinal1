bca8e051f03941329cc4f75f1c46735ea01405ed
David Nuñez <nundavid99@gmail.com>
2021-11-21 12:34:15 -0300

    Product service complete and addProduct modal working

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -9,8 +9,16 @@ import { ProductsService } from "src/app/services/products.service";
 export class ProductsComponent {
   public products: Array<any> = [];
   public showAddProductModal: Boolean = false;
+  public nuevoProd: any = {
+    nombre: '', existencia: 0, precio: 0
+  };
   async loadProducts(){
     this.products = await this._productsService.getProducts();
+  }
+  async addProduct(){
+    const newProduct = await this._productsService.addProduct(this.nuevoProd);
+    console.log(newProduct);
+    this.products.push(newProduct);
   }
   constructor(
     private _productsService: ProductsService,
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -12,4 +12,13 @@ export class ProductsService {
   public async getProducts(): Promise<any[]> {
     return await this._http.get<any>(this.urlApi).toPromise();
   }
+  public async addProduct(request: any): Promise<any> {
+    return await this._http.post<any>(this.urlApi, request).toPromise();
+  }
+  public async editProduct(request: any): Promise<any> {
+    return await this._http.put<any>(`${this.urlApi}/${request.id}`, request).toPromise();
+  }
+  public async deleteProduct( idProduct: Number): Promise<any[]> {
+    return await this._http.get<any>(this.urlApi + `/${idProduct}`).toPromise();
+  }
 }
